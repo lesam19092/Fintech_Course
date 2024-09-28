b3f301d1ad41f0ba7e06ae730880897584f032f2
lesam19092 <danigpro1337@gmail.com>
2024-09-28 22:03:23 +0300

    init commit

---
+++ src/Product.java
@@ -0,0 +1,15 @@
+
+public class Product {
+
+    private int id;
+    private String name;
+
+    private String description;
+
+    private Category category;
+
+    private String companyName;
+
+    private Double price;
+
+}
