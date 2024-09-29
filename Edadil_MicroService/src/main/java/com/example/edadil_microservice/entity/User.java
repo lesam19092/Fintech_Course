a2ded99428d699632754ac51523d548ce9df31cc
lesam19092 <danigpro1337@gmail.com>
2024-09-29 11:49:18 +0300

    add user.class

--- Edadil_MicroService/src/main/java/com/example/edadil_microservice/entity/User.java
+++ Edadil_MicroService/src/main/java/com/example/edadil_microservice/entity/User.java
@@ -1,12 +1,17 @@
 package com.example.edadil_microservice.entity;
 
-import com.example.edadil_microservice.Role;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
 
+
+@Data
 public class User {
 
     private Long id;
-
+    @Size(min = 2, message = "Не меньше 2 знаков")
+    private String username;
+    @Size(min = 2, message = "Не меньше 2 знаков")
     private String password;
-
+    private String passwordConfirm;
     private Role role;
 }
