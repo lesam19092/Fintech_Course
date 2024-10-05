0e276315f21e6619672e678707e3414f8a4c7aff
lesam19092 <danigpro1337@gmail.com>
2024-10-05 22:19:16 +0300

    добавил обработчик ошибок

--- Edadil_MicroService/src/main/java/com/example/edadil_microservice/EdadilMicroServiceApplication.java
+++ Edadil_MicroService/src/main/java/com/example/edadil_microservice/EdadilMicroServiceApplication.java
@@ -1,6 +1,5 @@
 package com.example.edadil_microservice;
 
-import com.example.edadil_microservice.service.UserServiceImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
