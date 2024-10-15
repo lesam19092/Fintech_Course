3800c6f94d17b2012a16f89c0443569ee856c3ab
lesam19092 <danigpro1337@gmail.com>
2024-10-15 13:21:46 +0300

    убрал импорт

--- Edadil_MicroService/src/main/java/com/example/edadil_microservice/service/company/CompanyServiceImpl.java
+++ Edadil_MicroService/src/main/java/com/example/edadil_microservice/service/company/CompanyServiceImpl.java
@@ -14,7 +14,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.Optional;
 import java.util.Set;
 
