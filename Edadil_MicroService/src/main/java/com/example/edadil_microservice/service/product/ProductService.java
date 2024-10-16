e077b1da6a3c8c2d4002f307ea794d7b2ff3868c
lesam19092 <danigpro1337@gmail.com>
2024-10-16 11:26:13 +0300

    добавил сервис для продуктов

--- Edadil_MicroService/src/main/java/com/example/edadil_microservice/service/product/ProductService.java
+++ Edadil_MicroService/src/main/java/com/example/edadil_microservice/service/product/ProductService.java
@@ -1,4 +1,11 @@
 package com.example.edadil_microservice.service.product;
 
+import com.example.edadil_microservice.model.entity.Product;
+
+import java.util.List;
+
 public interface ProductService {
+
+    List<Product> getAllProducts();
+
 }
--- Edadil_MicroService/src/main/java/com/example/edadil_microservice/service/product/ProductServiceImpl.java
+++ Edadil_MicroService/src/main/java/com/example/edadil_microservice/service/product/ProductServiceImpl.java
@@ -1,9 +1,15 @@
 package com.example.edadil_microservice.service.product;
 
 
+import com.example.edadil_microservice.model.entity.Product;
 import com.example.edadil_microservice.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static com.example.edadil_microservice.utils.EntityUtils.requireNonEmptyCollection;
+
 
 @Service
 @RequiredArgsConstructor
@@ -11,7 +17,11 @@ public class ProductServiceImpl implements ProductService {
 
 
     private final ProductRepository productRepository;
-    
+
+    @Override
+    public List<Product> getAllProducts() {
+        return requireNonEmptyCollection(productRepository.findAll());
+    }
 
 
 }
