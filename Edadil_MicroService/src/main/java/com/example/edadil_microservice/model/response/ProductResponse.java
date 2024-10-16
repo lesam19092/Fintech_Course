02bc46a785b7b599d649856b2fb1df8456dd0b0f
lesam19092 <danigpro1337@gmail.com>
2024-10-16 11:24:22 +0300

    изменил немного классы response

--- Edadil_MicroService/src/main/java/com/example/edadil_microservice/model/response/ProductResponse.java
+++ Edadil_MicroService/src/main/java/com/example/edadil_microservice/model/response/ProductResponse.java
@@ -10,4 +10,16 @@ public class ProductResponse {
     private String firm;
     private Integer count;
     private Double price;
+
+
+    //TODO потом убрать toString
+    @Override
+    public String toString() {
+        return "ProductResponse{" +
+                "name='" + name + '\'' +
+                ", firm='" + firm + '\'' +
+                ", count=" + count +
+                ", price=" + price +
+                '}';
+    }
 }
--- Edadil_MicroService/src/main/java/com/example/edadil_microservice/model/response/ShopResponse.java
+++ Edadil_MicroService/src/main/java/com/example/edadil_microservice/model/response/ShopResponse.java
@@ -10,5 +10,18 @@ public class ShopResponse {
     private String companyName;
     private int id;
     private String city;
+
+    //TODO потом убрать toString
+
+    @Override
+    public String toString() {
+        return "ShopResponse{" +
+                "companyName='" + companyName + '\'' +
+                ", id=" + id +
+                ", city='" + city + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+
     private String address;
 }
