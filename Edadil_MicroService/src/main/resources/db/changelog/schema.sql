5bd98dc9681d6f7aa313838e4527d855ba7a09b2
lesam19092 <danigpro1337@gmail.com>
2024-10-28 00:26:01 +0300

    исправил название таблиц

--- Edadil_MicroService/src/main/resources/db/changelog/schema.sql
+++ Edadil_MicroService/src/main/resources/db/changelog/schema.sql
@@ -1,16 +1,16 @@
-CREATE TABLE Companies
+CREATE TABLE companies
 (
     id           INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     company_name VARCHAR(100)
 );
 
-create table Firms
+create table firms
 (
     id           INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     firm_name VARCHAR(100)
 );
 
-CREATE TABLE Users
+CREATE TABLE users
 (
     id        INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     user_name VARCHAR(50),
@@ -18,30 +18,30 @@ CREATE TABLE Users
     password  VARCHAR(100)
 );
 
-CREATE TABLE Products
+CREATE TABLE products
 (
     id         INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name       VARCHAR(100),
     firm_id INT,
-    FOREIGN KEY (firm_id) REFERENCES Firms (id)
+    FOREIGN KEY (firm_id) REFERENCES firms (id)
 );
 
-CREATE TABLE Shops
+CREATE TABLE shops
 (
     id                 INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     address            VARCHAR(255),
     city               VARCHAR(100),
     name_of_company_id INT,
-    FOREIGN KEY (name_of_company_id) REFERENCES Companies (id)
+    FOREIGN KEY (name_of_company_id) REFERENCES companies (id)
 );
 
-CREATE TABLE ShopProduct
+CREATE TABLE shop_product
 (
     PRIMARY KEY (shop_id, product_id),
     shop_id INT,
     product_id INT,
     count INT   NOT NULL,
     price FLOAT NOT NULL,
-    FOREIGN KEY (shop_id) REFERENCES Shops (id),
-    FOREIGN KEY (product_id) REFERENCES Products (id)
+    FOREIGN KEY (shop_id) REFERENCES shops (id),
+    FOREIGN KEY (product_id) REFERENCES products (id)
 );
