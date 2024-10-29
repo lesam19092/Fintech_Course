79b2feca11c5728b68b2ff1e0514b069a7d8aa3e
lesam19092 <danigpro1337@gmail.com>
2024-10-29 23:13:07 +0300

    протестил работу отправки на почту

--- FoodRu_MicroService/src/main/java/org/example/foodru_microservice/controller/MealController.java
+++ FoodRu_MicroService/src/main/java/org/example/foodru_microservice/controller/MealController.java
@@ -1,15 +1,21 @@
 package org.example.foodru_microservice.controller;
 
 
+import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
-import org.example.foodru_microservice.mapper.MealMapper;
 import org.example.foodru_microservice.model.dto.MealDto;
 import org.example.foodru_microservice.model.dto.MealWithIngredientDto;
-import org.example.foodru_microservice.service.MealService;
+import org.example.foodru_microservice.service.meal.MealService;
+import org.example.foodru_microservice.service.mail.EmailService;
+import org.example.foodru_microservice.service.pdf.PdfService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.awt.*;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -18,6 +24,10 @@ public class MealController {
 
 
     private final MealService mealService;
+
+    private final EmailService emailService;
+
+    private final PdfService pdfService;
 
 
     @GetMapping("/meals")
@@ -32,6 +42,17 @@ public class MealController {
 
     @GetMapping("/meals/{id}/ingredients")
     public MealWithIngredientDto getMealIngredients(@PathVariable Integer id) {
-       return mealService.getMealsIngredients(id);
+        return mealService.getMealsIngredients(id);
+    }
+
+    @GetMapping("/mail")
+    public String getMail() throws MessagingException, IOException {
+
+       /* emailService.sendEmailWithAttachment("danigpro1337@gmail.com", "", "Test", "C:\\Users\\danil\\Desktop\\2305_L1_Палев_Гловацкий.pdf");
+        return "mail";*/
+
+        pdfService.savePdf(new ArrayList<>());
+
+        return "mail";
     }
 }
