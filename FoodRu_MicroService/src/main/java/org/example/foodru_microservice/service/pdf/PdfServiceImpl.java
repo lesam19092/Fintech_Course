c4ed40fe85a6aed4040605c33e2cedf3395fa64f
lesam19092 <danigpro1337@gmail.com>
2024-10-29 22:39:46 +0300

    перехожу к рефакторингу

--- FoodRu_MicroService/src/main/java/org/example/foodru_microservice/service/pdf/PdfServiceImpl.java
+++ FoodRu_MicroService/src/main/java/org/example/foodru_microservice/service/pdf/PdfServiceImpl.java
@@ -11,9 +11,8 @@ import org.example.foodru_microservice.service.upload.UploadService;
 import org.springframework.stereotype.Service;
 import com.lowagie.text.Font;
 
-import java.io.FileOutputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.util.List;
 
 @Service
@@ -22,32 +21,40 @@ public class PdfServiceImpl implements PdfService {
 
     private final UploadService uploadService;
 
-
     @Override
     public void savePdf(String filePath, List<Object> response) throws IOException {
-        try (Document document = new Document();
-             FileOutputStream fos = new FileOutputStream(filePath)) {
-            PdfPTable table = new PdfPTable(5);
-            PdfWriter.getInstance(document, fos);
+        try {
+            Document document = new Document();
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            PdfWriter.getInstance(document, baos);
             document.open();
-            BaseFont bfComic = BaseFont.createFont("C:\\Users\\danil\\Desktop\\Fintech_Course\\Edadil_MicroService\\arial.ttf", BaseFont.IDENTITY_H,
+            BaseFont bfComic = BaseFont.createFont("FoodRu_MicroService\\font\\arial.ttf", BaseFont.IDENTITY_H,
                     BaseFont.EMBEDDED);
             Font font = new Font(bfComic, 12);
 
+            PdfPTable table = new PdfPTable(5);
             table.addCell(new PdfPCell(new Phrase("название компании:", font)));
             table.addCell(new PdfPCell(new Phrase("адрес:", font)));
             table.addCell(new PdfPCell(new Phrase("список отсут продуктов:", font)));
             table.addCell(new PdfPCell(new Phrase("ингредиенты:", font)));
             table.addCell(new PdfPCell(new Phrase("стоимость:", font)));
-        /*for (PaymentReceipt p : response) {
-            table.addCell(new PdfPCell(new Phrase(p.getCompanyName(), font)));
-            table.addCell(new PdfPCell(new Phrase(p.getAddress(), font)));
-            table.addCell(new PdfPCell(new Phrase(p.getMissingIngredients().toString(), font)));
-            table.addCell(new PdfPCell(new Phrase(p.getIngredients().toString(), font)));
-            table.addCell(new PdfPCell(new Phrase(String.valueOf(p.getCost()), font)));
-        }*/
+            /*for (PaymentReceipt p : response) {
+                table.addCell(new PdfPCell(new Phrase(p.getCompanyName(), font)));
+                table.addCell(new PdfPCell(new Phrase(p.getAddress(), font)));
+                table.addCell(new PdfPCell(new Phrase(p.getMissingIngredients().toString(), font)));
+                table.addCell(new PdfPCell(new Phrase(p.getIngredients().toString(), font)));
+                table.addCell(new PdfPCell(new Phrase(String.valueOf(p.getCost()), font)));
+            }*/
             document.add(table);
-            uploadService.uploadPhoto(Files.readAllBytes(document.get));
+
+            document.close();
+
+            byte[] pdfBytes = baos.toByteArray();
+            System.out.println(pdfBytes.length);
+            uploadService.uploadPhoto(pdfBytes);
+        } catch (Exception exception) {
+            System.out.println("fdsfsd");
         }
+
     }
 }
