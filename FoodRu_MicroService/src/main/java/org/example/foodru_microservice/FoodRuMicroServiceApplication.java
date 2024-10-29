2d8f746505951aeeeb1265ccd77650782cdfb202
lesam19092 <danigpro1337@gmail.com>
2024-10-29 23:14:24 +0300

    добавил todo

--- FoodRu_MicroService/src/main/java/org/example/foodru_microservice/FoodRuMicroServiceApplication.java
+++ FoodRu_MicroService/src/main/java/org/example/foodru_microservice/FoodRuMicroServiceApplication.java
@@ -2,16 +2,15 @@ package org.example.foodru_microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "org.example.foodru_microservice.repository")
-
 public class FoodRuMicroServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FoodRuMicroServiceApplication.class, args);
     }
+    //TODO УБРАТЬ ПЕРЕМЕННЫЕ В COMPOSE.YML В ENV
 
 }
