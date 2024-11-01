2ff544f6e305ab43616fb55f7c387e7920c2e961
lesam19092 <danigpro1337@gmail.com>
2024-11-01 20:28:17 +0300

    добавил дто

---
+++ Edadil_MicroService/src/main/java/com/example/edadil_microservice/model/dto/ListIngredientDto.java
@@ -0,0 +1,15 @@
+package com.example.edadil_microservice.model.dto;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.ToString;
+
+import java.util.List;
+
+@Data
+@ToString
+public class ListIngredientDto {
+
+    private List<IngredientDto> ingredientDtoList;
+
+}
