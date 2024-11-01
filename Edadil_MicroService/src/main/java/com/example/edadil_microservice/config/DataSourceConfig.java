91602aec9b02d220b71f44d1b88b896416c707af
lesam19092 <danigpro1337@gmail.com>
2024-11-01 20:27:17 +0300

    подключи хикари

---
+++ Edadil_MicroService/src/main/java/com/example/edadil_microservice/config/DataSourceConfig.java
@@ -0,0 +1,18 @@
+package com.example.edadil_microservice.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Bean(name = "myDataSource")
+    @ConfigurationProperties("spring.datasource")
+    public DataSource myDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+}
