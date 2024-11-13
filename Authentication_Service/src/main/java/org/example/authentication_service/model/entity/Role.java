e72e577eecc77d9c53d39d4574a21c223ed1fa71
lesam19092 <danigpro1337@gmail.com>
2024-11-14 00:35:38 +0300

    добавил enum Для ролей

--- Authentication_Service/src/main/java/org/example/authentication_service/model/entity/Role.java
+++ Authentication_Service/src/main/java/org/example/authentication_service/model/entity/Role.java
@@ -2,9 +2,9 @@ package org.example.authentication_service.model.entity;
 
 import org.springframework.security.core.GrantedAuthority;
 
-public enum Role implements GrantedAuthority  {
-    ADMIN,
-    USER;
+public enum Role implements GrantedAuthority {
+    ROLE_ADMIN,
+    ROLE_USER;
 
     @Override
     public String getAuthority() {
