aa40df42321ad379d3ebcec4e6b4b30c700ad01d
lesam19092 <danigpro1337@gmail.com>
2024-11-13 13:30:47 +0300

    Добавил класс

--- Authentication_Service/src/main/java/org/example/authentication_service/AuthenticationServiceApplication.java
+++ Authentication_Service/src/main/java/org/example/authentication_service/AuthenticationServiceApplication.java
@@ -1,9 +1,11 @@
-package main.java.org.example.authentication_service;
+package org.example.authentication_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("org.example.authentication_service.repository")
 public class AuthenticationServiceApplication {
 
     public static void main(String[] args) {
