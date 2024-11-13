fa2648b79a1ffe908f46d9f7ed3a5d65732a23eb
lesam19092 <danigpro1337@gmail.com>
2024-11-14 00:33:39 +0300

    добавил репозитории

---
+++ Authentication_Service/src/main/java/org/example/authentication_service/repository/TokenRepository.java
@@ -0,0 +1,19 @@
+package org.example.authentication_service.repository;
+
+import org.example.authentication_service.model.entity.Token;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface TokenRepository extends JpaRepository<Token, Integer> {
+
+    @Query("""
+            select t from Token t join fetch t.user
+            where t.user.id = :userId and t.isLoggedOut = false
+            """)
+    List<Token> findAllAccessTokensByUser(Integer userId);
+
+    Optional<Token> findByAccessToken(String token);
+}
---
+++ Authentication_Service/src/main/java/org/example/authentication_service/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package org.example.authentication_service.repository;
+
+import org.example.authentication_service.model.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByName(String name);
+
+    @Query("SELECT u FROM User u JOIN FETCH u.instance WHERE u.name = :username AND u.instance.name = :instanceName")
+    Optional<User> findByUsernameAndInstance(String username, String instanceName);
+}
