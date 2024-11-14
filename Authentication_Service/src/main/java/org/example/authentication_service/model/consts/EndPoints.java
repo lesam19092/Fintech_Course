490cd42c7d123ffdd57425237b7b8229fe9170db
lesam19092 <danigpro1337@gmail.com>
2024-11-14 10:46:37 +0300

    добавил константы для эндпоинтов

--- Authentication_Service/src/main/java/org/example/authentication_service/model/consts/EndPoints.java
+++ Authentication_Service/src/main/java/org/example/authentication_service/model/consts/EndPoints.java
@@ -2,7 +2,11 @@ package org.example.authentication_service.model.consts;
 
 public interface EndPoints {
 
+    String AUTH = "/auth";
+    String LOGIN = "/login";
     String SECURED = "/secured";
+    String REGISTRATION = "/registration";
+    String RESET_PASSWORD = "/reset-password";
     String INFO = "/info";
     String ADMIN = "/admin";
     String LOGOUT = "/logout";
