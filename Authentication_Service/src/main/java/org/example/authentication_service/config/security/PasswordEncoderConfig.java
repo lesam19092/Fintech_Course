b8930612acd2053033f7943b890effdd48dcb563
lesam19092 <danigpro1337@gmail.com>
2024-11-15 18:32:57 +0300

    убрал его в другой package

--- Authentication_Service/src/main/java/org/example/authentication_service/config/security/PasswordEncoderConfig.java
+++ Authentication_Service/src/main/java/org/example/authentication_service/config/security/PasswordEncoderConfig.java
@@ -1,4 +1,4 @@
-package org.example.authentication_service.config;
+package org.example.authentication_service.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
