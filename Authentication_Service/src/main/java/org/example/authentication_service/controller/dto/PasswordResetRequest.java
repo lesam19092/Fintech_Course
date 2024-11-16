a2418e22c876295b66197dca5e9d61c967c5fb5b
lesam19092 <danigpro1337@gmail.com>
2024-11-16 21:35:55 +0300

    добаваил dto для снятия пароля

--- Authentication_Service/src/main/java/org/example/authentication_service/controller/dto/PasswordResetRequest.java
+++ Authentication_Service/src/main/java/org/example/authentication_service/controller/dto/PasswordResetRequest.java
@@ -1,25 +1,18 @@
 package org.example.authentication_service.controller.dto;
 
-import jakarta.validation.constraints.Size;
 import lombok.Data;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 
 
 @Data
 public class PasswordResetRequest {
 
-    @NotEmpty(message = "Name cannot be blank")
-    private String name;
+    @NotEmpty(message = "Email cannot be blank")
+    @Email(message = "Email should be valid")
+    private String email;
 
-    @NotEmpty(message = "New password cannot be blank")
-    @Size(min = 6, message = "New password must be at least 6 characters long")
-    private String newPassword;
-
-    @NotEmpty(message = "Confirm password cannot be blank")
-    @Size(min = 6, message = "Confirm password must be at least 6 characters long")
-    private String confirmPassword;
-
-    @NotEmpty(message = "Confirmation code cannot be blank")
-    private String confirmationCode;
+    @NotEmpty(message = "User type cannot be blank")
+    private String userType;
 }
