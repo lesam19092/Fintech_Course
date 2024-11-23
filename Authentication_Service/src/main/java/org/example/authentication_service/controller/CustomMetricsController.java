02ae538076fbabc5296908b0fba075aa36489598
lesam19092 <danigpro1337@gmail.com>
2024-11-23 21:16:50 +0300

    добавил через MDC

--- Authentication_Service/src/main/java/org/example/authentication_service/controller/CustomMetricsController.java
+++ Authentication_Service/src/main/java/org/example/authentication_service/controller/CustomMetricsController.java
@@ -2,10 +2,15 @@ package org.example.authentication_service.controller;
 
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
+
 @RestController
+@Slf4j
 public class CustomMetricsController {
 
     private final Counter customCounter;
@@ -15,8 +20,13 @@ public class CustomMetricsController {
     }
 
     @GetMapping("/custom")
-    public void incrementCustomMetric()
-    {
+    public void incrementCustomMetric() {
+        var requestId = UUID.randomUUID().toString();
+
+        try (var ignored = MDC.putCloseable("requestId", requestId + " я тут")) {
+            log.info("with structed log");
+        }
+
         customCounter.increment();
     }
 }
