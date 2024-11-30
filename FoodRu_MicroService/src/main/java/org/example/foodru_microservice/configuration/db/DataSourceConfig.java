c2ea65432b1bc92acdd1852cc3bb3569105cccd0
lesam19092 <danigpro1337@gmail.com>
2024-11-30 19:19:39 +0300

    поменял бин дата соурса

--- FoodRu_MicroService/src/main/java/org/example/foodru_microservice/configuration/db/DataSourceConfig.java
+++ FoodRu_MicroService/src/main/java/org/example/foodru_microservice/configuration/db/DataSourceConfig.java
@@ -11,7 +11,7 @@ import javax.sql.DataSource;
 @Configuration
 public class DataSourceConfig {
 
-    @Bean(name = "myDataSource")
+    @Bean(name = "foodRuDataBase")
     @ConfigurationProperties("spring.datasource")
     public DataSource myDataSource() {
         return DataSourceBuilder.create().build();
