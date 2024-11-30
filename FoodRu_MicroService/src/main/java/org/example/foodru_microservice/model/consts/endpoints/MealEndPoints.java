243ac2e8b1dd09e77e695c335258e665882868d0
lesam19092 <danigpro1337@gmail.com>
2024-11-30 11:18:13 +0300

    перенес в константы

--- FoodRu_MicroService/src/main/java/org/example/foodru_microservice/model/consts/endpoints/MealEndPoints.java
+++ FoodRu_MicroService/src/main/java/org/example/foodru_microservice/model/consts/endpoints/MealEndPoints.java
@@ -1,4 +1,16 @@
 package org.example.foodru_microservice.model.consts.endpoints;
 
 public interface MealEndPoints {
+
+    String MEALS = "/meals";
+
+    String MEAL = "/meals/{id}";
+
+    String MEAL_INGREDIENTS = "/meals/{id}/ingredients";
+
+    String MEAL_CHEAPEST = "/meals/{id}/ingredients/cheapest";
+
+    String USER_MEALS = "/get-meals";
+
+
 }
