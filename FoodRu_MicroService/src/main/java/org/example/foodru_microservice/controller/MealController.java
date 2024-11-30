79875dc1b8a97bbc39f5261074199423a4fad71d
lesam19092 <danigpro1337@gmail.com>
2024-12-01 00:13:17 +0300

    отрефакторил контроллер

--- FoodRu_MicroService/src/main/java/org/example/foodru_microservice/controller/MealController.java
+++ FoodRu_MicroService/src/main/java/org/example/foodru_microservice/controller/MealController.java
@@ -5,19 +5,11 @@ import lombok.RequiredArgsConstructor;
 import org.example.foodru_microservice.controller.dto.MealDto;
 import org.example.foodru_microservice.controller.dto.MealWithIngredientDto;
 import org.example.foodru_microservice.model.consts.endpoints.MealEndPoints;
-import org.example.foodru_microservice.service.kafka.KafkaProducer;
-import org.example.foodru_microservice.service.kafka.dto.IngredientDto;
-import org.example.foodru_microservice.service.kafka.dto.ListIngredientDto;
-import org.example.foodru_microservice.service.mail.EmailService;
 import org.example.foodru_microservice.service.meal.MealService;
-import org.example.foodru_microservice.service.pdf.PdfService;
-import org.example.foodru_microservice.service.user.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
-import java.security.Principal;
 import java.util.List;
 
 @RestController
@@ -26,14 +18,6 @@ public class MealController {
 
 
     private final MealService mealService;
-
-    private final EmailService emailService;
-
-    private final PdfService pdfService;
-
-    private final KafkaProducer kafkaProducer;
-
-    private final UserService userService;
 
 
     @GetMapping(MealEndPoints.MEALS)
@@ -58,30 +42,5 @@ public class MealController {
         return null;
     }
 
-    @GetMapping(MealEndPoints.USER_MEALS)
-    public List<MealDto> getAllMeals(Principal principal) {
-        return userService.getAllMeals(principal.getName());
-    }
 
-
-    @GetMapping("/mail/{email}")
-    public String getMail(@PathVariable String email) throws IOException {
-
-        ListIngredientDto listIngredientDto = new ListIngredientDto();
-
-        IngredientDto ingredientDto = IngredientDto.builder().name("Мясо").measure(1.0).build();
-        IngredientDto ingredientDto1 = IngredientDto.builder().name("гречка").measure(20.0).build();
-        IngredientDto ingredientDto2 = IngredientDto.builder().name("соль").measure(0.5).build();
-
-        listIngredientDto.setIngredientDtoList(List.of(ingredientDto, ingredientDto1, ingredientDto2));
-
-        kafkaProducer.sendMessage(listIngredientDto);
-        // pdfService.generateAndSendPdfReport(new ArrayList<>());
-        // emailService.sendEmailWithAttachment("danigpro1337@gmail.com", "C:\\Users\\danil\\Desktop\\2305_L1_Палев_Гловацкий.pdf");
-        return "mail";
-
-        //pdfService.savePdf(new ArrayList<>());*/
-
-
-    }
 }
