125c61429d53cb44b2aae4d2cc869048f5cee99c
lesam19092 <danigpro1337@gmail.com>
2024-11-30 20:24:13 +0300

    добавил user controller

--- FoodRu_MicroService/src/main/java/org/example/foodru_microservice/controller/UserController.java
+++ FoodRu_MicroService/src/main/java/org/example/foodru_microservice/controller/UserController.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.example.foodru_microservice.controller.dto.MealDto;
 import org.example.foodru_microservice.model.consts.endpoints.UserEndPoints;
 import org.example.foodru_microservice.service.user.UserService;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,22 +21,22 @@ public class UserController {
 
 
     @PostMapping(UserEndPoints.ADD_MEAL)
-    public ResponseEntity<String> addMeal(@PathVariable Long mealId, Principal principal) {
-        boolean added = userService.addMeal(mealId, principal.getName());
-        return ResponseEntity.ok(added ? "Meal added" : "Meal was already added");
+    public String addMeal(@PathVariable Long mealId, Principal principal) {
+        userService.addMeal(mealId, principal.getName());
+        return "Meal added";
     }
 
     @PostMapping(UserEndPoints.ADD_MEAL_TO_MENU)
-    public ResponseEntity<String> addMealToMenu(@PathVariable Long mealId, @PathVariable String menuName, Principal principal) {
-        boolean added = userService.addMealToMenu(mealId, menuName, principal.getName());
-        return ResponseEntity.ok(added ? "Meal added to menu" : "Meal was already in the menu");
+    public String addMealToMenu(@PathVariable Long mealId, @PathVariable String menuName, Principal principal) {
+        userService.addMealToMenu(mealId, menuName, principal.getName());
+        return "Meal added to menu";
     }
 
 
     @PostMapping(UserEndPoints.CREATE_MENU)
-    public ResponseEntity<String> createMenu(Principal principal, @PathVariable String menuName) {
-        boolean created = userService.createMenu(principal.getName(), menuName);
-        return ResponseEntity.ok(created ? "Menu created" : "Menu was already created");
+    public String createMenu(Principal principal, @PathVariable String menuName) {
+        userService.createMenu(principal.getName(), menuName);
+        return "Menu created";
     }
 
     @GetMapping(UserEndPoints.USER_MEALS)
