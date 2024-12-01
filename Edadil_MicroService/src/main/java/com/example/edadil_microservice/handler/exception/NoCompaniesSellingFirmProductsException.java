3052f3591bcdf4767a127f48136e6656f46a3bbc
lesam19092 <danigpro1337@gmail.com>
2024-12-02 01:42:33 +0300

    добавил исключение + добавил обработчик

---
+++ Edadil_MicroService/src/main/java/com/example/edadil_microservice/handler/exception/NoCompaniesSellingFirmProductsException.java
@@ -0,0 +1,7 @@
+package com.example.edadil_microservice.handler.exception;
+
+public class NoCompaniesSellingFirmProductsException extends RuntimeException {
+  public NoCompaniesSellingFirmProductsException(String message) {
+    super(message);
+  }
+}
