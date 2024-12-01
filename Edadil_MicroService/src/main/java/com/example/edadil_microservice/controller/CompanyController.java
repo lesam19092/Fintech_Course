6cadc6801ce32f43fa6d61acda340ce7213fc8a6
lesam19092 <danigpro1337@gmail.com>
2024-12-01 18:21:35 +0300

    отреакторил CompanyController

--- Edadil_MicroService/src/main/java/com/example/edadil_microservice/controller/CompanyController.java
+++ Edadil_MicroService/src/main/java/com/example/edadil_microservice/controller/CompanyController.java
@@ -1,16 +1,15 @@
 package com.example.edadil_microservice.controller;
 
 
-import com.example.edadil_microservice.model.entity.Company;
-import com.example.edadil_microservice.model.response.ShopProductResponse;
-import com.example.edadil_microservice.model.response.ShopResponse;
+import com.example.edadil_microservice.controller.dto.CompanyDto;
 import com.example.edadil_microservice.service.company.CompanyService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.Set;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,36 +20,14 @@ public class CompanyController {
 
 
     @GetMapping("/companies")
-    public List<Company> getAllCompany() {
+    public List<CompanyDto> getAllCompany() {
         return companyService.findAllCompanies();
     }
 
     @GetMapping("/companies/{companyId}")
-    public Company getCompanyById(@PathVariable Integer companyId) {
+    public CompanyDto getCompanyById(@PathVariable Integer companyId) {
         return companyService.findCompanyById(companyId);
     }
 
-
-    @GetMapping("/companies/{companyId}/shops")
-    public Set<ShopResponse> getCompanyShops(@PathVariable Integer companyId) {
-        return companyService.findCompanyShops(companyId);
-    }
-
-    @GetMapping("/companies/{companyId}/shops/{city}")
-    public Set<ShopResponse> getCompanyShopsByCity(@PathVariable Integer companyId, @PathVariable String city) {
-        return companyService.findCompanyShopsInCity(companyId, city);
-    }
-
-    @GetMapping("/companies/{companyId}/shops/{city}/{shopId}")
-    public ShopResponse getCompanyShopFromCityById(@PathVariable Integer companyId, @PathVariable String city, @PathVariable Integer shopId) {
-        return companyService.findCompanyShopInCityById(companyId, city, shopId);
-    }
-
-
-    @GetMapping("/companies/{companyId}/shops/{city}/{shopId}/products")
-    public ShopProductResponse getCompanyShopProducts(@PathVariable Integer companyId, @PathVariable String city, @PathVariable Integer shopId) {
-        return companyService.retrieveShopProducts(companyId, city, shopId);
-    }
-    
 
 }
