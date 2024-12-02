749cc56aaa410b3a258b6d4c96fa8a31bc179442
lesam19092 <danigpro1337@gmail.com>
2024-12-02 22:53:31 +0300

    немного изменил работу репозитория

--- Edadil_MicroService/src/main/java/com/example/edadil_microservice/repository/ShopProductRepository.java
+++ Edadil_MicroService/src/main/java/com/example/edadil_microservice/repository/ShopProductRepository.java
@@ -6,18 +6,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface ShopProductRepository extends JpaRepository<ShopProduct, Integer> {
     @EntityGraph(attributePaths = {"shop", "shop.nameOfCompany", "product", "product.firm"})
     List<ShopProduct> findShopProductsByProductId(Integer productId);
 
-    @Query("SELECT sp FROM ShopProduct sp JOIN FETCH sp.shop s JOIN FETCH s.nameOfCompany c JOIN FETCH sp.product p JOIN FETCH p.firm f WHERE f.id = :firmId AND c.id = :companyId")
+    @Query("SELECT DISTINCT sp FROM ShopProduct sp JOIN FETCH sp.shop s JOIN FETCH s.nameOfCompany c JOIN FETCH sp.product p JOIN FETCH p.firm f WHERE f.id = :firmId AND c.id = :companyId")
     List<ShopProduct> findShopProductsByFirmIdAndCompanyId(Integer firmId, Integer companyId);
 
     @Query("SELECT sp FROM ShopProduct sp WHERE sp.shop.nameOfCompany.id = :companyId AND sp.shop.city = :city AND sp.shop.id = :shopId")
-    Optional<ShopProduct> findShopProductsByCompanyIdAndCityAndShopId(Integer companyId, String city, Integer shopId);
+    List<ShopProduct> findShopProductsByCompanyIdAndCityAndShopId(Integer companyId, String city, Integer shopId);
 
     @Query("SELECT sp FROM ShopProduct sp JOIN FETCH sp.shop s JOIN FETCH s.nameOfCompany c JOIN FETCH sp.product p JOIN FETCH p.firm f WHERE f.id = :firmId AND c.id = :companyId AND s.id = :shopId")
-    Optional<ShopProduct> findProductsInShopByFirmAndCompany(Integer firmId, Integer companyId, Integer shopId);
+    List<ShopProduct> findProductsInShopByFirmAndCompany(Integer firmId, Integer companyId, Integer shopId);
 }
