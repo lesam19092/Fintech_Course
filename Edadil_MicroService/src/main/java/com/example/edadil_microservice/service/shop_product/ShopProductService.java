946aaaaf31bff750d47523eb5b879f46a5e6b753
lesam19092 <danigpro1337@gmail.com>
2024-12-02 23:24:19 +0300

    отрефакторил сервис + добавил метод для получения всех продуктов у всех магазинов

--- Edadil_MicroService/src/main/java/com/example/edadil_microservice/service/shop_product/ShopProductService.java
+++ Edadil_MicroService/src/main/java/com/example/edadil_microservice/service/shop_product/ShopProductService.java
@@ -5,9 +5,12 @@ import com.example.edadil_microservice.controller.dto.ShopProductDto;
 import java.util.List;
 
 public interface ShopProductService {
-    List<ShopProductDto> retrieveShopProducts(Integer companyId, String city, Integer shopId);
+    ShopProductDto retrieveShopProducts(Integer companyId, String city, Integer shopId);
 
     List<ShopProductDto> findShopsSellingProduct(Integer firmId, Integer productId);
 
-    List<ShopProductDto> findProductsInShopByFirmAndCompany(Integer firmId, Integer companyId, Integer shopId);
+    ShopProductDto findProductsInShopByFirmAndCompany(Integer firmId, Integer companyId, Integer shopId);
+
+    List<ShopProductDto> getAllShopsWithProducts();
+
 }
