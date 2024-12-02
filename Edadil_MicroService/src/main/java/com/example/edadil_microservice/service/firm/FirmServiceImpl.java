93c026b63b6b4ca0c7472b29241f4b429a5bfeff
lesam19092 <danigpro1337@gmail.com>
2024-12-02 15:20:37 +0300

    FirmServiceImpl Отерфакторил

--- Edadil_MicroService/src/main/java/com/example/edadil_microservice/service/firm/FirmServiceImpl.java
+++ Edadil_MicroService/src/main/java/com/example/edadil_microservice/service/firm/FirmServiceImpl.java
@@ -1,7 +1,9 @@
 package com.example.edadil_microservice.service.firm;
 
 import com.example.edadil_microservice.controller.dto.FirmDto;
+import com.example.edadil_microservice.handler.exception.EntityNotFoundException;
 import com.example.edadil_microservice.mapper.FirmMapper;
+import com.example.edadil_microservice.model.entity.Firm;
 import com.example.edadil_microservice.repository.FirmRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,7 +13,6 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 import static com.example.edadil_microservice.utils.EntityUtils.requireNonEmptyCollection;
-import static com.example.edadil_microservice.utils.EntityUtils.requirePresentEntity;
 
 @Service
 @RequiredArgsConstructor
@@ -34,9 +35,11 @@ public class FirmServiceImpl implements FirmService {
     @Override
     public FirmDto findFirmById(Integer firmId) {
         log.info("Fetching firm with ID: {}", firmId);
-        return firmMapper.toDto(requirePresentEntity(
-                firmRepository.findById(firmId))
-        );
+        return firmMapper.toDto(getFirmById(firmId));
     }
 
+    private Firm getFirmById(Integer firmId) {
+        return firmRepository.findById(firmId)
+                .orElseThrow(() -> new EntityNotFoundException("Firm not found with ID: " + firmId));
+    }
 }
