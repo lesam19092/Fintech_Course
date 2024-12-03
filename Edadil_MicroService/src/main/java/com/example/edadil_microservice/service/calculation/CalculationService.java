233122ad8983f8d6b5a18b5c872b196ef4b8d3ab
lesam19092 <danigpro1337@gmail.com>
2024-12-04 00:15:07 +0300

    отрефакторил CalculationService

--- Edadil_MicroService/src/main/java/com/example/edadil_microservice/service/calculation/CalculationService.java
+++ Edadil_MicroService/src/main/java/com/example/edadil_microservice/service/calculation/CalculationService.java
@@ -1,16 +1,15 @@
 package com.example.edadil_microservice.service.calculation;
 
-import com.example.edadil_microservice.model.dto.IngredientDto;
-import com.example.edadil_microservice.model.response.PaymentReceiptResponse;
+import com.example.edadil_microservice.controller.dto.IngredientDto;
+import com.example.edadil_microservice.controller.response.PaymentReceiptResponse;
 
-import java.io.IOException;
 import java.util.List;
 
 public interface CalculationService {
 
-    List<PaymentReceiptResponse> generatePaymentReceipt(List<IngredientDto> response) throws IOException;
+    List<PaymentReceiptResponse> generatePaymentReceipt(List<IngredientDto> response);
 
     List<PaymentReceiptResponse> getPaymentsWithOutMissingIngredients(List<IngredientDto> response);
 
-    List<PaymentReceiptResponse>  getTheCheapestPayments(List<IngredientDto> response);
+    PaymentReceiptResponse getTheCheapestPayments(List<IngredientDto> response);
 }
--- Edadil_MicroService/src/main/java/com/example/edadil_microservice/service/calculation/CalculationServiceImpl.java
+++ Edadil_MicroService/src/main/java/com/example/edadil_microservice/service/calculation/CalculationServiceImpl.java
@@ -1,18 +1,20 @@
+
 package com.example.edadil_microservice.service.calculation;
 
-import com.example.edadil_microservice.exception.EmptyResultException;
-import com.example.edadil_microservice.model.dto.IngredientDto;
-import com.example.edadil_microservice.model.response.IngredientResponse;
-import com.example.edadil_microservice.model.response.PaymentReceiptResponse;
-import com.example.edadil_microservice.model.response.ProductResponse;
-import com.example.edadil_microservice.model.response.ShopProductResponse;
-import com.example.edadil_microservice.service.company.CompanyService;
+import com.example.edadil_microservice.controller.dto.IngredientDto;
+import com.example.edadil_microservice.controller.dto.ProductDetailsDto;
+import com.example.edadil_microservice.controller.dto.ShopProductDto;
+import com.example.edadil_microservice.controller.response.IngredientResponse;
+import com.example.edadil_microservice.controller.response.PaymentReceiptResponse;
+import com.example.edadil_microservice.handler.exception.EmptyResultException;
 import com.example.edadil_microservice.service.kafka.KafkaProducer;
+import com.example.edadil_microservice.service.shop_product.ShopProductService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
@@ -27,88 +29,96 @@ public class CalculationServiceImpl implements CalculationService {
     //TODO ЗАПОЛНИТЬ ТАБЛИЦУ ДАННЫМИ
 
 
-    private final CompanyService companyService;
+    private final ShopProductService shopProductService;
 
     private final KafkaProducer kafkaProducer;
 
 
     @Override
-    public List<PaymentReceiptResponse> generatePaymentReceipt(List<IngredientDto> response) throws IOException {
-        List<ShopProductResponse> allShopsProducts = companyService.getAllShopsWithProducts();
-
+    public List<PaymentReceiptResponse> generatePaymentReceipt(List<IngredientDto> response) {
+        List<ShopProductDto> allShopsProducts = shopProductService.getAllShopsWithProducts();
         List<PaymentReceiptResponse> payments = allShopsProducts.stream()
                 .map(shop -> createPayment(shop, response))
                 .toList();
-
-        log.info("Generated {} ingredient responses", payments.size());
-
+        log.info("Generated {} payment receipts", payments.size());
         return requireNonEmptyCollection(payments);
     }
 
 
     @Override
     public List<PaymentReceiptResponse> getPaymentsWithOutMissingIngredients(List<IngredientDto> requests) {
-        List<ShopProductResponse> allShopsProducts = companyService.getAllShopsWithProducts();
-
+        List<ShopProductDto> allShopsProducts = shopProductService.getAllShopsWithProducts();
         List<PaymentReceiptResponse> ingredientResponses = allShopsProducts.stream()
                 .map(shop -> createPayment(shop, requests))
-                .filter(payment -> payment.getMissingIngredients().isEmpty())
                 .toList();
-
-        log.info("Generated {} ingredient responses", ingredientResponses.size());
-        return requireNonEmptyCollection(ingredientResponses);
+        log.info("Generated {} ingredient responses without missing ingredients", ingredientResponses.size());
+        return ingredientResponses;
     }
 
     @Override
-    public List<PaymentReceiptResponse> getTheCheapestPayments(List<IngredientDto> requests) {
-
+    @Transactional
+    public PaymentReceiptResponse getTheCheapestPayments(List<IngredientDto> requests) {
         List<PaymentReceiptResponse> ingredientResponses = getPaymentsWithOutMissingIngredients(requests);
-
-        List<PaymentReceiptResponse> cheapestPayments = ingredientResponses.stream()
-                .filter(ir -> ir.getCost() == ingredientResponses.stream()
-                        .min(Comparator.comparingDouble(PaymentReceiptResponse::getCost))
-                        .orElseThrow(() -> new EmptyResultException("No valid payments found"))
-                        .getCost())
-                .toList();
-
-        kafkaProducer.sendMessage(cheapestPayments.get(0));
-
-
-        return requireNonEmptyCollection(cheapestPayments);
+        PaymentReceiptResponse cheapestPayment = findCheapestPayment(ingredientResponses);
+        kafkaProducer.sendMessage(cheapestPayment);
+        return cheapestPayment;
     }
 
-    //TODO может быть добавить среднюю стоимость для каждой компании
-
-
-    private PaymentReceiptResponse createPayment(ShopProductResponse shop, List<IngredientDto> response) {
-        log.debug("Creating ingredient response for shop ID: {}", shop.getShop().getId());
-        PaymentReceiptResponse paymentReceiptResponse = new PaymentReceiptResponse();
-        paymentReceiptResponse.setAddress(shop.getShop().getCity() + " , " + shop.getShop().getAddress());
-        paymentReceiptResponse.setCompanyName(shop.getShop().getCompanyName());
-
-        response.forEach(item -> processIngredient(item, shop, paymentReceiptResponse));
-        return paymentReceiptResponse;
+    private PaymentReceiptResponse findCheapestPayment(List<PaymentReceiptResponse> responses) {
+        return responses.stream()
+                .filter(response -> response.getCost() > 0)
+                .min(Comparator.comparingDouble(PaymentReceiptResponse::getCost))
+                .orElseThrow(() -> new EmptyResultException("No valid payments found"));
     }
 
-    private void processIngredient(IngredientDto item, ShopProductResponse shop, PaymentReceiptResponse paymentReceiptResponse) {
-        boolean found = shop.getProducts().stream()
-                .filter(product -> product.getName().equals(item.getName()) && product.getCount() >= item.getMeasure())
-                .peek(product -> {
-                    log.debug("Found matching product: {} with sufficient count: {}", product.getName(), product.getCount());
-                    IngredientResponse ingredient = createIngredientResponse(item, product);
-                    paymentReceiptResponse.addIngredient(ingredient);
-                    paymentReceiptResponse.setCost(product.getPrice(), item.getMeasure());
-                })
+
+    private PaymentReceiptResponse createPayment(ShopProductDto shop, List<IngredientDto> ingredients) {
+        log.debug("Creating payment receipt for shop ID: {}", shop.getShop().getId());
+        PaymentReceiptResponse paymentReceipt = initializePaymentReceipt(shop);
+        ingredients.forEach(ingredient -> processIngredient(ingredient, shop, paymentReceipt));
+        return paymentReceipt;
+    }
+
+    private PaymentReceiptResponse initializePaymentReceipt(ShopProductDto shop) {
+        return PaymentReceiptResponse.builder()
+                .address(shop.getShop().getCity() + ", " + shop.getShop().getAddress())
+                .companyName(shop.getShop().getCompanyName())
+                .ingredients(new ArrayList<>())
+                .missingIngredients(new ArrayList<>())
+                .cost(0.0)
+                .build();
+    }
+
+
+    private void processIngredient(IngredientDto ingredient, ShopProductDto shop, PaymentReceiptResponse paymentReceipt) {
+        shop.getProducts().stream()
+                .filter(product -> isMatchingProduct(product, ingredient))
                 .findFirst()
-                .isPresent();
-
-        if (!found) {
-            log.warn("Missing ingredient: {}", item.getName());
-            paymentReceiptResponse.addMissingIngredient(item.getName());
-        }
+                .ifPresentOrElse(
+                        product -> addIngredientToReceipt(product, ingredient, paymentReceipt),
+                        () -> addMissingIngredientToReceipt(ingredient, paymentReceipt)
+                );
     }
 
-    private IngredientResponse createIngredientResponse(IngredientDto item, ProductResponse product) {
+    private boolean isMatchingProduct(ProductDetailsDto product, IngredientDto ingredient) {
+        return product.getName().equalsIgnoreCase(ingredient.getName())
+                && product.getCount() >= ingredient.getMeasure();
+    }
+
+    private void addIngredientToReceipt(ProductDetailsDto product, IngredientDto ingredient, PaymentReceiptResponse paymentReceipt) {
+        log.debug("Found matching product: {}", product.getName());
+
+        IngredientResponse ingredientResponse = createIngredientResponse(ingredient, product);
+        paymentReceipt.addIngredient(ingredientResponse);
+        paymentReceipt.setCost(product.getPrice(), ingredient.getMeasure());
+    }
+
+    private void addMissingIngredientToReceipt(IngredientDto ingredient, PaymentReceiptResponse paymentReceipt) {
+        log.warn("Missing ingredient: {}", ingredient.getName());
+        paymentReceipt.addMissingIngredient(ingredient.getName());
+    }
+
+    private IngredientResponse createIngredientResponse(IngredientDto item, ProductDetailsDto product) {
         return new IngredientResponse(item.getName(),
                 item.getMeasure(),
                 product.getFirm(),
