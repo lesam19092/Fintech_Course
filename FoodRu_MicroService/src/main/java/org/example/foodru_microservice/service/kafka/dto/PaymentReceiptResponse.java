de8d9d951071fd98c95a4aa87be95f97a6ba9be5
lesam19092 <danigpro1337@gmail.com>
2024-12-03 22:44:35 +0300

    отрефакторил класс + добавил константу

--- FoodRu_MicroService/src/main/java/org/example/foodru_microservice/service/kafka/dto/PaymentReceiptResponse.java
+++ FoodRu_MicroService/src/main/java/org/example/foodru_microservice/service/kafka/dto/PaymentReceiptResponse.java
@@ -11,39 +11,10 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class PaymentReceiptResponse {
-
-
     private String companyName;
     private String address;
     private List<IngredientResponse> ingredients = new ArrayList<>();
     private boolean hasMissingIngredients;
     private List<String> missingIngredients = new ArrayList<>();
-    private Double cost;
-
-
-    public void addIngredient(IngredientResponse ingredient) {
-
-        this.ingredients.add(ingredient);
-    }
-
-    public void addMissingIngredient(String ingredientName) {
-        this.hasMissingIngredients = true;
-        this.missingIngredients.add(ingredientName);
-    }
-
-    public void setCost(double costs, double count) {
-        this.cost += costs * count;
-    }
-
-    @Override
-    public String toString() {
-        return "PaymentReceipt{" +
-                "companyName='" + companyName + '\'' +
-                ", address='" + address + '\'' +
-                ", ingredients=" + ingredients +
-                ", hasMissingIngredients=" + hasMissingIngredients +
-                ", missingIngredients=" + missingIngredients +
-                ", cost=" + cost +
-                '}';
-    }
+    private double cost;
 }
