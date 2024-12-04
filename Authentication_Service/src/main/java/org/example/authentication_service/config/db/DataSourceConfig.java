4844b5e8898da63befe07d35613532722aca74f9
lesam19092 <danigpro1337@gmail.com>
2024-12-04 10:54:31 +0300

    вношу изменения

--- Authentication_Service/src/main/java/org/example/authentication_service/config/db/DataSourceConfig.java
+++ Authentication_Service/src/main/java/org/example/authentication_service/config/db/DataSourceConfig.java
@@ -1,4 +1,4 @@
-package org.example.authentication_service.config.db;
+package com.example.edadil_microservice.config.db;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
@@ -10,9 +10,10 @@ import javax.sql.DataSource;
 @Configuration
 public class DataSourceConfig {
 
-    @Bean(name = "authDataSource")
+    @Bean(name = "edadilDataSource")
     @ConfigurationProperties("spring.datasource")
     public DataSource myDataSource() {
         return DataSourceBuilder.create().build();
     }
+
 }
