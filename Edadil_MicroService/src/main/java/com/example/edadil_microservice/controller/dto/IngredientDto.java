e5b27fa12eccfc88b311f9e1151d428ae3ebaa3a
lesam19092 <danigpro1337@gmail.com>
2024-12-08 22:45:10 +0300

    добавил логи и графану на все 3 сервиса

--- Edadil_MicroService/src/main/java/com/example/edadil_microservice/controller/dto/IngredientDto.java
+++ Edadil_MicroService/src/main/java/com/example/edadil_microservice/controller/dto/IngredientDto.java
@@ -2,9 +2,11 @@ package com.example.edadil_microservice.controller.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class IngredientDto {
     private String name;
     private Double measure;
